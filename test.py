5882852b436e5c38459f2ab18102f1057ad9ebc7
Viha27 <vihanga.18@itfac.mrt.ac.lk>
2021-09-02 13:07:12 +0530

    .gitignore file added

--- test.py
+++ test.py
@@ -7,13 +7,13 @@ from app import app
 def test1():
     """A dummy docstring."""
     response = app.test_client().get("/")
-    assert response.status_code == 500
+    assert response.status_code == 200
 
 
 def test2():
     """A dummy docstring."""
     response = app.test_client().get("/edit")
-    assert response.status_code == 500
+    assert response.status_code == 200
 
 
 def test3():
